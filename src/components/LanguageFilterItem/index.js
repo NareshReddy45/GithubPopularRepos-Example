948127e7da02b3bdeed1927ebc5c945964e31306
joshnareddy <joshna.reddy@financepeer.co>
2025-02-23 15:23:35 +0530

    master

--- src/components/LanguageFilterItem/index.js
+++ src/components/LanguageFilterItem/index.js
@@ -1,25 +1,27 @@
 import './index.css'
 
-const LanguageFilterItem = ({
+function LanguageFilterItem({
   languageFiltersData,
   activeLanguageId,
   setActiveLanguage,
-}) => (
-  <ul className="language-filter-container">
-    {languageFiltersData.map(item => (
-      <li key={item.id}>
-        <button
-          type="button"
-          className={`language-button ${
-            item.id === activeLanguageId ? 'active' : ''
-          }`}
-          onClick={() => setActiveLanguage(item.id)}
-        >
-          {item.language}
-        </button>
-      </li>
-    ))}
-  </ul>
-)
+}) {
+  return (
+    <ul className="language-filter-container">
+      {languageFiltersData.map(item => (
+        <li key={item.id}>
+          <button
+            type="button"
+            className={`language-button ${
+              item.id === activeLanguageId ? 'active' : ''
+            }`}
+            onClick={() => setActiveLanguage(item.id)}
+          >
+            {item.language}
+          </button>
+        </li>
+      ))}
+    </ul>
+  )
+}
 
 export default LanguageFilterItem
--- src/components/RepositoryItem/index.js
+++ src/components/RepositoryItem/index.js
@@ -1,41 +1,43 @@
 import './index.css'
 
-const RepositoryItem = ({repositories}) => (
-  <div className="cards-container">
-    {repositories.map(item => (
-      <div key={item.id} className="item-card">
-        <img src={item.avatarUrl} alt={item.name} className="url-image" />
-        <h1 className="heading">{item.name}</h1>
+function RepositoryItem({repositories}) {
+  return (
+    <div className="cards-container">
+      {repositories.map(item => (
+        <div key={item.id} className="item-card">
+          <img src={item.avatarUrl} alt={item.name} className="url-image" />
+          <h1 className="heading">{item.name}</h1>
 
-        <div className="count-direction">
-          <img
-            src="https://assets.ccbp.in/frontend/react-js/stars-count-img.png"
-            alt="stars"
-            className="count-image"
-          />
-          <p>{item.starsCount} stars</p>
-        </div>
+          <div className="count-direction">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/stars-count-img.png"
+              alt="stars"
+              className="count-image"
+            />
+            <p>{item.starsCount} stars</p>
+          </div>
 
-        <div className="count-direction">
-          <img
-            src="https://assets.ccbp.in/frontend/react-js/forks-count-img.png"
-            alt="forks"
-            className="count-image"
-          />
-          <p>{item.forksCount} forks</p>
-        </div>
+          <div className="count-direction">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/forks-count-img.png"
+              alt="forks"
+              className="count-image"
+            />
+            <p>{item.forksCount} forks</p>
+          </div>
 
-        <div className="count-direction">
-          <img
-            src="https://assets.ccbp.in/frontend/react-js/issues-count-img.png"
-            alt="open issues"
-            className="count-image"
-          />
-          <p>{item.issuesCount} open issues</p>
+          <div className="count-direction">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/issues-count-img.png"
+              alt="open issues"
+              className="count-image"
+            />
+            <p>{item.issuesCount} open issues</p>
+          </div>
         </div>
-      </div>
-    ))}
-  </div>
-)
+      ))}
+    </div>
+  )
+}
 
 export default RepositoryItem
